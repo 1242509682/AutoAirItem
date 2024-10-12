35857ab11b8be3797da9dd3fca3640e0d7a1ad56
羽 学 <1242509682@qq.com>
2024-10-12 17:02:44 +0800

    添加项目文件。

---
+++ Commands.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using TShockAPI;
+
+namespace AutoAirItem;
+
+    public class Commands
+    {
+        public static void AirCmd(CommandArgs args)
+        {
+            var name = args.Player.Name;
+            var config = AutoAirItem.Config.Items.FirstOrDefault(item => item.Name == name);
+
+            if (!AutoAirItem.Config.Open) return;
+            if (config == null)
+            {
+                args.Player.SendErrorMessage("请用角色进入服务器后输入：/air 指令查看菜单");
+                return;
+            }
+
+            if (args.Parameters.Count == 0)
+            {
+                HelpCmd(args.Player);
+                args.Player.SendSuccessMessage($"您的垃圾桶启用状态为：[c/92C5EC:{config.Enabled}]");
+                if (!config.Enabled)
+                {
+                    args.Player.SendSuccessMessage($"请输入该指令开启→: [c/92C5EC:/air on] ");
+                }
+                else
+                {
+                    args.Player.SendSuccessMessage($"请输入该指令添加物品→: [c/92C5EC:/air add 土块] ");
+                }
+                return;
+            }
+
+            if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "list")
+            {
+                args.Player.SendInfoMessage($"[{config.Name}的垃圾桶]\n" + string.Join(", ", config.ItemName.Select(x => "[c/92C5EC:{0}]".SFormat(x))));
+                return;
+            }
+
+            if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "on")
+            {
+                bool isEnabled = config.Enabled;
+                config.Enabled = !isEnabled;
+                string Mess = isEnabled ? "禁用" : "启用";
+                args.Player.SendSuccessMessage($"玩家 [{args.Player.Name}] 已[c/92C5EC:{Mess}]自动垃圾桶功能。");
+                AutoAirItem.Config.Write();
+                return;
+            }
+
+            if (args.Parameters.Count == 1 && args.Parameters[0].ToLower() == "clear")
+            {
+                config.ItemName.Clear();
+                args.Player.SendSuccessMessage($"已清理[c/92C5EC: {args.Player.Name} ]的垃圾桶");
+                AutoAirItem.Config.Write();
+                return;
+            }
+
+            if (args.Parameters.Count == 2)
+            {
+                Item item;
+                List<Item> Items = TShock.Utils.GetItemByIdOrName(args.Parameters[1]);
+                if (Items.Count > 1)
+                {
+                    args.Player.SendMultipleMatchError(Items.Select(i => i.Name));
+                    return;
+                }
+
+                if (Items.Count == 0)
+                {
+                    args.Player.SendErrorMessage("不存在该物品，\"物品查询\": \"[c/92C5EC:https://terraria.wiki.gg/zh/wiki/Item_IDs]\"");
+                    return;
+                }
+
+                else
+                    item = Items[0];
+
+                switch (args.Parameters[0].ToLower())
+                {
+                    case "add":
+                        {
+                            if (config.ItemName.Contains(item.Name))
+                            {
+                                args.Player.SendErrorMessage("物品 [c/92C5EC:{0}] 已在垃圾桶中!", item.Name);
+                                return;
+                            }
+                            config.ItemName.Add(item.Name);
+                            AutoAirItem.Config.Write();
+                            args.Player.SendSuccessMessage("已成功将物品添加到垃圾桶: [c/92C5EC:{0}]!\n" + $"您的垃圾桶启用状态为：[c/92C5EC:{config.Enabled}]", item.Name);
+                            break;
+                        }
+                    case "delete":
+                    case "del":
+                    case "remove":
+                        {
+                            if (!config.ItemName.Contains(item.Name))
+                            {
+                                args.Player.SendErrorMessage("物品 {0} 不在垃圾桶中!", item.Name);
+                                return;
+                            }
+                            config.ItemName.Remove(item.Name);
+                            AutoAirItem.Config.Write();
+                            args.Player.SendSuccessMessage("已成功从垃圾桶删除物品: [c/92C5EC:{0}]!\n" + $"您的垃圾桶启用状态为：[c/92C5EC:{config.Enabled}]", item.Name);
+                            break;
+                        }
+
+                    default:
+                        {
+                            HelpCmd(args.Player);
+                            break;
+                        }
+                }
+            }
+        }
+
+        #region 菜单方法
+        private static void HelpCmd(TSPlayer player)
+        {
+            if (player == null) return;
+            else
+            {
+                player.SendMessage("【自动垃圾桶】指令菜单\n" +
+                 "/air —— 查看垃圾桶菜单\n" +
+                 "/air on —— 开启|关闭垃圾桶功能\n" +
+                 "/air list —— 列出自己的垃圾桶\n" +
+                 "/air clear —— 清理垃圾桶\n" +
+                 "/air add 或 del 物品名字 —— 添加|删除《自动垃圾桶》的物品", Color.AntiqueWhite);
+            }
+        }
+        #endregion
+    }
---
+++ Configuration.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using TShockAPI;
+
+namespace AutoAirItem;
+
+    internal class Configuration
+    {
+        #region 实例变量
+        [JsonProperty("插件指令权限", Order = -1)]
+        public string Text { get; set; } = "指令菜单：/air 或 /垃圾，权限名【AutoAir.use】，给玩家权限：/group addperm default AutoAir.use";
+
+        [JsonProperty("插件开关", Order = 0)]
+        public bool Open { get; set; } = true;
+
+        [JsonProperty("自动清理提示", Order = 0)]
+        public bool Mess { get; set; } = true;
+
+        [JsonProperty("清理垃圾速度", Order = 1)]
+        public int UpdateRate = 60;
+
+        [JsonProperty("玩家数据表", Order = 2)]
+        public List<ItemData> Items { get; set; } = new List<ItemData>();
+        #endregion
+
+        #region 预设参数方法
+        public void Ints()
+        {
+            Items = new List<ItemData>
+            {
+                new ItemData("羽学",false,default,new List<string>()),
+            };
+        }
+        #endregion
+
+        #region 数据结构
+        public class ItemData
+        {
+            [JsonProperty("玩家名字", Order = 1)]
+            public string Name { get; set; }
+            [JsonProperty("垃圾桶开关", Order = 2)]
+            public bool Enabled { get; set; } = false;
+            [JsonProperty("登录时间", Order = 3)]
+            public DateTime LoginTime { get; set; }
+            [JsonProperty("垃圾桶物品", Order = 5)]
+            public List<string> ItemName { get; set; } = new List<string>();
+
+            public ItemData(string name = "", bool enabled = false, DateTime time = default,List<string> item = null!)
+            {
+                Name = name ?? "";
+                Enabled = enabled;
+                LoginTime = time;
+                ItemName = item;
+            }
+        }
+        #endregion
+
+        #region 读取与创建配置文件方法
+        public static readonly string FilePath = Path.Combine(TShock.SavePath, "自动垃圾桶.json");
+
+        public void Write()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(FilePath, json);
+        }
+
+        public static Configuration Read()
+        {
+            if (!File.Exists(FilePath))
+            {
+                var NewConfig = new Configuration();
+                NewConfig.Ints();
+                new Configuration().Write();
+                return NewConfig;
+            }
+            else
+            {
+                string jsonContent = File.ReadAllText(FilePath);
+                return JsonConvert.DeserializeObject<Configuration>(jsonContent)!;
+            }
+        }
+        #endregion
+
+    }
