987318cddc5a0c7f005c1cf988c6f8fc681813e8
羽 学 <1242509682@qq.com>
2024-10-12 23:43:38 +0800

    update v1.0.1 加入清理数据周期

--- AutoAirItem.cs
+++ AutoAirItem.cs
@@ -5,125 +5,146 @@ using TShockAPI;
 using TShockAPI.Hooks;
 
 
-namespace AutoAirItem
+namespace AutoAirItem;
+
+[ApiVersion(2, 1)]
+public class AutoAirItem : TerrariaPlugin
 {
-    [ApiVersion(2, 1)]
-    public class AutoAirItem : TerrariaPlugin
+
+    #region 插件信息
+    public override string Name => "自动垃圾桶";
+    public override string Author => "羽学";
+    public override Version Version => new Version(1, 0, 1);
+    public override string Description => "涡轮增压不蒸鸭";
+    #endregion
+
+    #region 注册与释放
+    public AutoAirItem(Main game) : base(game) { }
+    private GeneralHooks.ReloadEventD _reloadHandler;
+    public override void Initialize()
     {
-
-        #region 插件模版信息
-        public override string Name => "自动垃圾桶";
-        public override string Author => "羽学";
-        public override Version Version => new Version(1, 0, 0);
-        public override string Description => "涡轮增压不蒸鸭";
-        #endregion
-
-        #region 注册与释放
-        public AutoAirItem(Main game) : base(game) { }
-        private GeneralHooks.ReloadEventD _reloadHandler;
-        public override void Initialize()
-        {
-            LoadConfig();
-            this._reloadHandler = (_) => LoadConfig();
-            GeneralHooks.ReloadEvent += this._reloadHandler;
-            ServerApi.Hooks.GameUpdate.Register(this, this.OnGameUpdate);
-            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
-            TShockAPI.Commands.ChatCommands.Add(new Command("AutoAir.use", Commands.AirCmd, "air", "垃圾"));
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                GeneralHooks.ReloadEvent -= this._reloadHandler;
-                ServerApi.Hooks.GameUpdate.Deregister(this, this.OnGameUpdate);
-                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
-                TShockAPI.Commands.ChatCommands.RemoveAll(x => x.CommandDelegate == Commands.AirCmd);
-            }
-            base.Dispose(disposing);
-        }
-        #endregion
-
-        #region 配置重载读取与写入方法
-        internal static Configuration Config = new();
-        private static void LoadConfig()
-        {
-            Config = Configuration.Read();
-            Config.Write();
-            TShock.Log.ConsoleInfo("[自动垃圾桶]重新加载配置完毕。");
-        }
-        #endregion
-
-        #region 玩家加入更新配置方法
-        private void OnJoin(JoinEventArgs args)
-        {
-            var plr = TShock.Players[args.Who];
-            var list = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
-
-            if (!Config.Open) return;
-            if (!Config.Items.Any(item => item.Name == plr.Name))
-            {
-                Config.Items.Add(new Configuration.ItemData()
-                {
-                    Name = plr.Name,
-                    Enabled = false,
-                    LoginTime = DateTime.Now,
-                    ItemName = new List<string>()
-                });
-            }
-            else
-            {
-                list!.LoginTime = DateTime.Now;
-            }
-            Config.Write();
-        }
-        #endregion
-
-        #region 触发自动垃圾桶
-        public static long Timer = 0L;
-        private void OnGameUpdate(EventArgs args)
-        {
-            Timer++;
-
-            if (!Config.Open) return;
-
-            foreach (var plr in TShock.Players.Where(plr => plr != null && plr.Active && plr.IsLoggedIn))
-            {
-                var list = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
-                if (list != null && list.Enabled && Timer % Config.UpdateRate == 0)
-                {
-                    AutoAirItems(plr, list.ItemName);
-                }
-            }
-        }
-        #endregion
-
-        #region 自动清理物品方法
-        public static bool AutoAirItems(TSPlayer plr, List<string> List)
-        {
-            if (!plr.IsLoggedIn || plr == null) return false;
-
-            Player player = plr.TPlayer;
-
-            for (int i = 0; i < player.inventory.Length; i++)
-            {
-                var item = player.inventory[i];
-                var id = TShock.Utils.GetItemById(item.type).netID;
-                if (item != null && List.Contains(item.Name) && item.Name != player.inventory[player.selectedItem].Name)
-                {
-                    item.TurnToAir();
-                    plr.SendData(PacketTypes.PlayerSlot, null, plr.Index, PlayerItemSlotID.Inventory0 + i);
-
-                    if (Config.Mess)
-                    {
-                        var itemName = Lang.GetItemNameValue(id);
-                        plr.SendMessage($"【自动垃圾桶】已将 '[c/92C5EC:{itemName}]'从您的背包中移除", 255, 246, 158);
-                    }
-                    return true;
-                }
-            }
-            return false;
-        }
-        #endregion
+        LoadConfig();
+        this._reloadHandler = (_) => LoadConfig();
+        GeneralHooks.ReloadEvent += this._reloadHandler;
+        ServerApi.Hooks.GameUpdate.Register(this, this.OnGameUpdate);
+        ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+        ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
+        TShockAPI.Commands.ChatCommands.Add(new Command("AutoAir.use", Commands.AirCmd, "air", "垃圾"));
     }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            GeneralHooks.ReloadEvent -= this._reloadHandler;
+            ServerApi.Hooks.GameUpdate.Deregister(this, this.OnGameUpdate);
+            ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+            ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
+            TShockAPI.Commands.ChatCommands.RemoveAll(x => x.CommandDelegate == Commands.AirCmd);
+        }
+        base.Dispose(disposing);
+    }
+    #endregion
+
+    #region 配置重载读取与写入方法
+    internal static Configuration Config = new();
+    private static void LoadConfig()
+    {
+        Config = Configuration.Read();
+        Config.Write();
+        TShock.Log.ConsoleInfo("[自动垃圾桶]重新加载配置完毕。");
+    }
+    #endregion
+
+    #region 玩家更新配置方法（计入记录时间并创建配置结构）
+    private void OnJoin(JoinEventArgs args)
+    {
+        var plr = TShock.Players[args.Who];
+        var list = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
+
+        if (!Config.Open) return;
+        if (!Config.Items.Any(item => item.Name == plr.Name))
+        {
+            Config.Items.Add(new Configuration.ItemData()
+            {
+                Name = plr.Name,
+                Enabled = false,
+                LoginTime = DateTime.Now,
+                ItemName = new List<string>()
+            });
+        }
+        else
+        {
+            list!.LoginTime = DateTime.Now;
+        }
+        Config.Write();
+    }
+    #endregion
+
+    #region 玩家离开服务器更新记录时间，横比所有玩家的记录时间，如超过清理周期，则自动删除玩家数据
+    private void OnLeave(LeaveEventArgs args)
+    {
+        var plr = TShock.Players[args.Who];
+        var list = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
+        if (Config.Items.Any(item => item.Name == plr.Name))
+        {
+            list!.LoginTime = DateTime.Now;
+        }
+
+        var Remove = Config.Items.Where(list =>
+            list.LoginTime != default && (DateTime.Now - list.LoginTime).TotalHours >= Config.timer).ToList();
+
+        foreach (var plr2 in Remove)
+        {
+            Config.Items.Remove(plr2);
+        }
+
+        Config.Write();
+    }
+    #endregion
+
+    #region 触发自动垃圾桶
+    public static long Timer = 0L;
+    private void OnGameUpdate(EventArgs args)
+    {
+        Timer++;
+
+        if (!Config.Open) return;
+
+        foreach (var plr in TShock.Players.Where(plr => plr != null && plr.Active && plr.IsLoggedIn))
+        {
+            var list = Config.Items.FirstOrDefault(x => x.Name == plr.Name);
+            if (list != null && list.Enabled && Timer % Config.UpdateRate == 0)
+            {
+                AutoAirItems(plr, list.ItemName);
+            }
+        }
+    }
+    #endregion
+
+    #region 自动清理物品方法
+    public static bool AutoAirItems(TSPlayer plr, List<string> List)
+    {
+        Player player = plr.TPlayer;
+
+        for (int i = 0; i < player.inventory.Length; i++)
+        {
+            var item = player.inventory[i];
+            var id = TShock.Utils.GetItemById(item.type).netID;
+            if (item != null && List.Contains(item.Name) && item.Name != player.inventory[player.selectedItem].Name)
+            {
+                item.TurnToAir();
+                plr.SendData(PacketTypes.PlayerSlot, null, plr.Index, PlayerItemSlotID.Inventory0 + i);
+
+                if (Config.Mess)
+                {
+                    var itemName = Lang.GetItemNameValue(id);
+                    plr.SendMessage($"【自动垃圾桶】已将 '[c/92C5EC:{itemName}]'从您的背包中移除", 255, 246, 158);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    #endregion
 }
--- Configuration.cs
+++ Configuration.cs
@@ -3,81 +3,87 @@ using TShockAPI;
 
 namespace AutoAirItem;
 
-    internal class Configuration
+internal class Configuration
+{
+    #region 实例变量
+    [JsonProperty("插件指令权限", Order = -16)]
+    public string Text { get; set; } = "指令菜单：/air 或 /垃圾，权限名【AutoAir.use】，给玩家权限：/group addperm default AutoAir.use";
+
+    [JsonProperty("使用说明", Order = -15)]
+    public string Text2 { get; set; } = "玩家每次进出服都会更新【记录时间】，玩家A离线时间与玩家B登录时间相差超过【清理周期】所设定的时间，则自动清理该玩家A的数据";
+
+    [JsonProperty("插件开关", Order = -14)]
+    public bool Open { get; set; } = true;
+
+    [JsonProperty("清理数据周期/小时", Order = 1)]
+    public long timer { get; set; } = 24;
+
+    [JsonProperty("自动清理提示", Order = 2)]
+    public bool Mess { get; set; } = true;
+
+    [JsonProperty("清理垃圾速度", Order = 3)]
+    public int UpdateRate = 60;
+
+    [JsonProperty("玩家数据表", Order = 4)]
+    public List<ItemData> Items { get; set; } = new List<ItemData>();
+    #endregion
+
+    #region 预设参数方法
+    public void Ints()
     {
-        #region 实例变量
-        [JsonProperty("插件指令权限", Order = -1)]
-        public string Text { get; set; } = "指令菜单：/air 或 /垃圾，权限名【AutoAir.use】，给玩家权限：/group addperm default AutoAir.use";
-
-        [JsonProperty("插件开关", Order = 0)]
-        public bool Open { get; set; } = true;
-
-        [JsonProperty("自动清理提示", Order = 0)]
-        public bool Mess { get; set; } = true;
-
-        [JsonProperty("清理垃圾速度", Order = 1)]
-        public int UpdateRate = 60;
-
-        [JsonProperty("玩家数据表", Order = 2)]
-        public List<ItemData> Items { get; set; } = new List<ItemData>();
-        #endregion
-
-        #region 预设参数方法
-        public void Ints()
-        {
-            Items = new List<ItemData>
+        Items = new List<ItemData>
             {
                 new ItemData("羽学",false,default,new List<string>()),
             };
-        }
-        #endregion
-
-        #region 数据结构
-        public class ItemData
-        {
-            [JsonProperty("玩家名字", Order = 1)]
-            public string Name { get; set; }
-            [JsonProperty("垃圾桶开关", Order = 2)]
-            public bool Enabled { get; set; } = false;
-            [JsonProperty("登录时间", Order = 3)]
-            public DateTime LoginTime { get; set; }
-            [JsonProperty("垃圾桶物品", Order = 5)]
-            public List<string> ItemName { get; set; } = new List<string>();
-
-            public ItemData(string name = "", bool enabled = false, DateTime time = default,List<string> item = null!)
-            {
-                Name = name ?? "";
-                Enabled = enabled;
-                LoginTime = time;
-                ItemName = item;
-            }
-        }
-        #endregion
-
-        #region 读取与创建配置文件方法
-        public static readonly string FilePath = Path.Combine(TShock.SavePath, "自动垃圾桶.json");
-
-        public void Write()
-        {
-            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
-            File.WriteAllText(FilePath, json);
-        }
-
-        public static Configuration Read()
-        {
-            if (!File.Exists(FilePath))
-            {
-                var NewConfig = new Configuration();
-                NewConfig.Ints();
-                new Configuration().Write();
-                return NewConfig;
-            }
-            else
-            {
-                string jsonContent = File.ReadAllText(FilePath);
-                return JsonConvert.DeserializeObject<Configuration>(jsonContent)!;
-            }
-        }
-        #endregion
-
     }
+    #endregion
+
+    #region 数据结构
+    public class ItemData
+    {
+        [JsonProperty("玩家名字", Order = 1)]
+        public string Name { get; set; }
+        [JsonProperty("记录时间", Order = 2)]
+        public DateTime LoginTime { get; set; }
+        [JsonProperty("垃圾桶开关", Order = 3)]
+        public bool Enabled { get; set; } = false;
+        [JsonProperty("垃圾桶物品", Order = 4)]
+        public List<string> ItemName { get; set; } = new List<string>();
+
+        public ItemData(string name = "", bool enabled = false, DateTime time = default, List<string> item = null!)
+        {
+            Name = name ?? "";
+            Enabled = enabled;
+            LoginTime = time;
+            ItemName = item;
+        }
+    }
+    #endregion
+
+    #region 读取与创建配置文件方法
+    public static readonly string FilePath = Path.Combine(TShock.SavePath, "自动垃圾桶.json");
+
+    public void Write()
+    {
+        string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+        File.WriteAllText(FilePath, json);
+    }
+
+    public static Configuration Read()
+    {
+        if (!File.Exists(FilePath))
+        {
+            var NewConfig = new Configuration();
+            NewConfig.Ints();
+            new Configuration().Write();
+            return NewConfig;
+        }
+        else
+        {
+            string jsonContent = File.ReadAllText(FilePath);
+            return JsonConvert.DeserializeObject<Configuration>(jsonContent)!;
+        }
+    }
+    #endregion
+
+}
